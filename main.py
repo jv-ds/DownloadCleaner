99df4015417f2adda2f16c618a149e1ba12f62b6
Jeevan <jvet0005@student.monash.edu>
2024-04-03 16:32:56 +1100

    Added if name == main method for watchdog API

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 source_dir = "/Users/jeevan/Downloads"
 
@@ -9,3 +14,20 @@ def print_filenames(folder):
 
 
 print_filenames(os.scandir(source_dir))
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir       # to track Downloads folder
+    logging.info(f'start watching directory {path!r}')
+    event_handler = LoggingEventHandler()       #should change to function later
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
